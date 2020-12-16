8dbefbad33ada7f5720c16fe166cfde8257d6d0a
hjp1011 <hjp1011@163.com>
2020-12-16 23:12:42 +0800

    update

--- config/index.config.js
+++ config/index.config.js
@@ -2,16 +2,16 @@ const CONFIG = {
     // 开发环境配置
     development: {
         assetsPath: '/static', // 静态资源路径
-        baseUrl: 'http://xxx.com', // 后台接口请求地址
-        hostUrl: 'http://localhost:8080', // H5地址(前端运行地址)
+        baseUrl: '', // 后台接口请求地址
+        hostUrl: '', // H5地址(前端运行地址)
         weixinAppId: '' // 微信公众号appid
 
     },
     // 生产环境配置
     production: {
         assetsPath: '/static', // 静态资源路径
-        baseUrl: 'http://xxx.com', // 后台接口请求地址
-        hostUrl: 'http://xxx.com', // H5地址(前端运行地址)
+        baseUrl: '', // 后台接口请求地址
+        hostUrl: '', // H5地址(前端运行地址)
         weixinAppId: '' // 微信公众号appid
 
     }
